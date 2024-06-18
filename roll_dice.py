3769596884b8cffd8713dc317419c2c6c8209731
Alban343 <bauduin.alban@gmail.com>
2024-06-18 13:57:11 +0200

    new file:   P_Bobby.txt #	new file:   P_Pavard.txt #	new file:   P_Ronaldinho.txt #	modified:   roll_dice.py #

--- roll_dice.py
+++ roll_dice.py
@@ -1,9 +1,26 @@
 import numpy as np
+import pandas as pd
+
+# PLAYERS
+players = ['Bobby', 'Pavard', 'Ronaldinho']
+P_stats_d = {}
+for p in players:
+    stat = open(str(f'P_{p}.txt'), 'r')
+    line = []
+    for l in stat:
+        line.append(l.replace('\n',''))
+    P_stats_d[line[0]] = [line[1].split(',')[1],line[2].split(',')[1],line[3].split(',')[1]]
+    stat.close()
+
+P_stats = pd.DataFrame(P_stats_d)
+print(P_stats)
 
 # ROLL THE DICES
 def RollD():
     result = np.random.randint(1,7)
     return result
+
+Starting_dices = 6
 
 def Init_Roll(starting_dices):
     score = []
@@ -12,4 +29,3 @@ def Init_Roll(starting_dices):
         score.append(d)
     print(score)
 
-Init_Roll(6)
