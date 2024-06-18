3c8add08d3513a15cc11e63e20ec7f519bc1c36c
Alban343 <bauduin.alban@gmail.com>
2024-06-18 16:01:41 +0200

    modified:   P_Bobby.txt #	modified:   P_Mec_Raide.txt #	modified:   P_Pavard.txt #	modified:   P_Ronaldinho.txt #	modified:   roll_dice.py

--- roll_dice.py
+++ roll_dice.py
@@ -9,24 +9,60 @@ for p in players:
     line = []
     for l in stat:
         line.append(l.replace('\n',''))
-    P_stats_d[line[0]] = [30, int(line[1].split(',')[1]), int(line[2].split(',')[1]), int(line[3].split(',')[1])]
+    P_stats_d[line[0]] = [30, bool(int(line[1].split(',')[1])), int(line[2].split(',')[1]), int(line[3].split(',')[1])]
     stat.close()
 col_names = ['health', 'cautious','consistent','attack']
 P_stats = pd.DataFrame(P_stats_d, index=col_names)
+print(P_stats)
 
 # ROLL THE DICES
 def RollD():
     result = np.random.randint(1,7)
     return result
 
-Starting_dices = 6
-
-def Init_Roll(starting_dices):
+starting_dices = 6
+def Hand_Roll(starting_dices):
     score = []
     for d in range(starting_dices):
         d = RollD()
         score.append(d)
     return score
 
+# DECISIONS FILTER
 
-print(Init_Roll(Starting_dices))
+def filter_hi(list):
+    count_ones = list.count(1)
+    count_sixes = list.count(6)
+    
+    if count_ones > count_sixes:
+        most_frequent = 1
+        less_frequent = 6
+    elif count_sixes > count_ones:
+        most_frequent = 6
+        less_frequent = 1
+    else:
+        if np.random.rand() < 0.5:
+            most_frequent = 1
+        else:
+            most_frequent = 6
+
+    filtered_up = [num for num in list if num == most_frequent]
+    filtered_down = [num for num in list if num == less_frequent]
+
+    return filtered_up, filtered_down
+
+
+
+# PLAYER PLAYS
+player = 'Pavard'
+def player_plays(player):
+    choix = []
+    res = Hand_Roll(starting_dices)
+    decision = filter_hi(res)
+    print(decision)
+
+    for d in res:
+        print(d)
+        choix.append(d)
+
+player_plays(player)
