3f6882a3b793a4103069401e70b3b373a72be60d
Alban343 <bauduin.alban@gmail.com>
2024-06-18 19:26:54 +0200

    modified:   roll_dice.py

--- roll_dice.py
+++ roll_dice.py
@@ -30,9 +30,48 @@ def Hand_Roll(starting_dices):
 
 # DECISIONS FILTER
 
-def filter_hi(list):
+def filter_lo_no_up(list, player):
+    count_twos = list.count(2)
+    count_fives = list.count(5)
+    print(count_twos, count_fives)
+    if count_twos == 0 and count_fives == 0:
+        return ['X']
+    
+    if count_twos > count_fives:
+        most_frequent = 2
+        less_frequent = 5
+    elif count_fives > count_twos:
+        most_frequent = 5
+        less_frequent = 2
+    else:
+        if np.random.rand() < 0.5:
+            most_frequent = 2
+            less_frequent = 5
+        else:
+            most_frequent = 5
+            less_frequent = 2
+
+    filtered_lo_up = []
+    for num in list:
+        if num == most_frequent:
+            filtered_lo_up.append(num)
+    filtered_lo_down = []
+    for num in list:
+        if num == less_frequent:
+            filtered_lo_down.append(num)
+
+    if filtered_lo_down == []:
+        return filtered_lo_up
+    else:
+        return filtered_lo_up, filtered_lo_down
+
+
+def filter_hi(list, player):
     count_ones = list.count(1)
     count_sixes = list.count(6)
+
+    if count_ones == 0 and count_sixes == 0:
+        return ['X']
     
     if count_ones > count_sixes:
         most_frequent = 1
@@ -43,26 +82,68 @@ def filter_hi(list):
     else:
         if np.random.rand() < 0.5:
             most_frequent = 1
+            less_frequent = 6
         else:
             most_frequent = 6
+            less_frequent = 1
 
-    filtered_up = [num for num in list if num == most_frequent]
-    filtered_down = [num for num in list if num == less_frequent]
+    filtered_hi_up = []
+    for num in list:
+        if num == most_frequent:
+            filtered_hi_up.append(num)
+    filtered_hi_down = []
+    for num in list:
+        if num == less_frequent:
+            filtered_hi_down.append(num)
 
-    return filtered_up, filtered_down
+    if P_stats.loc['cautious', player]:
+        return filtered_hi_up
+    else:
+        if filtered_hi_down == []:
+            return filtered_hi_up
+        else:
+            if P_stats.loc['consistent', player] >= np.random.rand()*10:
+                return filtered_hi_up
+            else:
+                return filtered_hi_down
 
 
 
 # PLAYER PLAYS
-player = 'Pavard'
+
+player = 'Ronaldinho'
 def player_plays(player):
+    hidot_bet = True
+    if np.random.rand() < 0.5:
+        hidot_bet = False
     choix = []
     res = Hand_Roll(starting_dices)
-    decision = filter_hi(res)
-    print(decision)
+    decision = filter_hi(res, player)
+    
+    if decision == ['X']:
+        decision = filter_lo_no_up(res, player)
+        if decision == ['X']:
+            if hidot_bet == False:
+                choix.append(3)
+            if hidot_bet == True:
+                choix.append(4)
+        else:
+            print(f'le joueur va devoir se contenter de pas grand chose : {decision}')
+            #Coder la fonction pour choisir 5 ou 2
+    else:
+        for d in decision:
+            choix.append(d)
+        #Coder ici une propriété de consistent pour quelqu'un qui voudrait ajouter un dé assez fort
+        
+        # 2ND RUN
+        print(f'le joueur a choisi : {choix}')
+        if np.mean(choix) < 3:
+            hidot_bet = False
+        if np.mean(choix) > 4:
+            hidot_bet = True
+    print(hidot_bet)
 
     for d in res:
         print(d)
-        choix.append(d)
 
 player_plays(player)
